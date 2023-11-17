8366bee81af1eceba3b1aaf6071baa2bd116ea50
Mubai Hua <hmbyxfyj@gmail.com>
2023-11-17 14:53:50 -0800

    add testing api under excelinterface app to test the connection between django and fuseki server

--- backend/excelinterface/views.py
+++ backend/excelinterface/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from pyfuseki import FusekiUpdate, FusekiQuery
 
-# Create your views here.
+
+class HelloWorldAPIView(APIView):
+    def get(self, request, format=None):
+        fuseki_update = FusekiUpdate('http://localhost:3030', 'message')
+        fuseki_query = FusekiQuery('http://localhost:3030', 'message')
+        sparql_str = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX ex: <http://example.com/>
+SELECT ?from ?msg ?subj
+WHERE {
+  ?msg rdf:type ex:msg ;
+       ex:from ?from ;
+       ex:to "bob" ;
+       ex:subj ?subj .
+}
+        """
+        query_result = fuseki_query.run_sparql(sparql_str)
+        response_json = query_result.convert()
+        print(response_json)
+        return Response({'message': 'Hello, world!'})
