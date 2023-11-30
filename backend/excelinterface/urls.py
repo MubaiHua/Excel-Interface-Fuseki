ef4876634d53df074b2b5f74942867e5133510cd
Mubai Hua <hmbyxfyj@gmail.com>
2023-11-30 00:01:32 -0800

    Convert api in excelinterface into api_view

--- backend/excelinterface/urls.py
+++ backend/excelinterface/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HelloWorldAPIView
+from .views import get, i_want_my_own_name
 
 urlpatterns = [
-    path('hello/', HelloWorldAPIView.as_view(), name='hello'),
+    path('get/', get, name='get'),
+    path('i_want_my_own_name/', i_want_my_own_name, name='i_want_my_own_name')
     # other paths specific to your app...
 ]
