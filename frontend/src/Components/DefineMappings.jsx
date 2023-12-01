3d9dc8ac6d2366421f0ad980b759d34cb9e9baf0
Melody Cai <melodycai@g.ucla.edu>
2023-11-30 16:17:14 -0800

    print the names of databases in frontend

--- frontend/src/Components/DefineMappings.jsx
+++ frontend/src/Components/DefineMappings.jsx
@@ -1,15 +1,17 @@
 import { getFusekiDatasets } from '../Utils/FusekiAPI';
 import React, { useEffect, useState } from 'react';
+import { MainContext } from '../MainContext';
 
 const DefineMappings = () => {
   const [datasets, setDatasets] = useState([]);
   const [error, setError] = useState('');
+  const { userName } = useState(MainContext);
 
   useEffect(() => {
     getFusekiDatasets()
         .then(response => {
           console.log("API Response:", response);
-          setDatasets(response.datasets);
+          setDatasets(response);
         })
         .catch(err => {
           console.error('Error fetching datasets:', err);
@@ -20,6 +22,23 @@ const DefineMappings = () => {
   if (error) {
     return <div>Error: {error}</div>;
   }
+  return (
+    <div>
+      <h1>Welcome Mapping Administrator
+          {' '}
+          { userName }
+      </h1>
+      <h1>
+          Please select the database you would like to access:
+      </h1>
+      <h1>Fuseki Datasets</h1>
+      {datasets.map((dataset, index) => (
+        <div key={index}>
+          <h2>Dataset Name: {dataset}</h2>
+        </div>
+      ))}
+    </div>
+  );
 };
 //   return (
 //     <div>
