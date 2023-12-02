f5ac1e133145721883b347e655d054f49b70ac31
Mubai Hua <hmbyxfyj@gmail.com>
2023-12-02 12:57:20 -0800

    update NavBar to show username

--- frontend/src/Components/DefineMappings.jsx
+++ frontend/src/Components/DefineMappings.jsx
@@ -1,17 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import {
-  Typography,
-  Select,
-  MenuItem,
-  Grid,
-} from '@mui/material';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Grid from '@mui/material/Grid';
 import PropTypes from 'prop-types';
 import { getFusekiDatasets } from '../Utils/FusekiAPI';
 
-function DefineMappings({ userID, userName }) {
+const steps = ['Select Database', 'Select Data Types', 'Select Attributes of the type'];
+
+export default function DefineMappings({ userName, userID }) {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+
   const [datasets, setDatasets] = useState([]);
   const [selectedDataset, setSelectedDataset] = useState('');
-  const [error, setError] = useState('');
 
   useEffect(() => {
     getFusekiDatasets()
@@ -21,7 +29,7 @@ function DefineMappings({ userID, userName }) {
       })
       .catch((err) => {
         console.error('Error fetching datasets:', err);
-        setError('Failed to load datasets');
+        alert('Failed to load datasets');
       });
     console.log(userID);
   }, [userID]);
@@ -30,46 +38,120 @@ function DefineMappings({ userID, userName }) {
     setSelectedDataset(event.target.value);
   };
 
-  if (error) {
-    return (
-      <div>
-        Error:
-        {error}
-      </div>
-    );
-  }
+  const isStepOptional = (step) => step === 114514; // place holder for feature
+
+  const isStepSkipped = (step) => skipped.has(step);
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
 
   return (
-    <>
-      <Typography variant="h3" align="center" gutterBottom>
-        Welcome Mapping Administrator
-        {' '}
-        {userName}
-      </Typography>
-      <Typography variant="h4" align="center" gutterBottom>
-        Please select the database you would like to access:
-      </Typography>
+    <Box sx={{ width: '100%' }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps = {};
+          const labelProps = {};
+          if (isStepOptional(index)) {
+            labelProps.optional = (
+              <Typography variant="caption">Optional</Typography>
+            );
+          }
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <>
+          <Typography sx={{ mt: 2, mb: 1 }}>
+            This is your query
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleReset}>Create a new Mapping</Button>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Grid container style={{ height: '70%' }}>
+            <Grid item xs={6} style={{ borderRight: '1px solid #ccc', padding: '10px' }}>
+              <Typography variant="h4" align="center" gutterBottom>
+                Please select the database you would like to access:
+              </Typography>
+              <Typography variant="h5">Fuseki Datasets</Typography>
+              <Select value={selectedDataset} onChange={handleDatasetChange} fullWidth>
+                {datasets.map((dataset, index) => (
+                  <MenuItem key={index} value={dataset}>
+                    {dataset}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item xs={6} style={{ padding: '10px' }}>
+              {/* Add content for the right half of the page */}
+            </Grid>
+          </Grid>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mr: 1 }}
+            >
+              Back
+            </Button>
+            <Box sx={{ flex: '1 1 auto' }} />
+            {isStepOptional(activeStep) && (
+              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                Skip
+              </Button>
+            )}
 
-      <Grid container style={{ height: '70%' }}>
-        <Grid item xs={6} style={{ borderRight: '1px solid #ccc', padding: '10px' }}>
-          <Typography variant="h5">Fuseki Datasets</Typography>
-          <Select value={selectedDataset} onChange={handleDatasetChange} fullWidth>
-            {datasets.map((dataset, index) => (
-              <MenuItem key={index} value={dataset}>
-                {dataset}
-              </MenuItem>
-            ))}
-          </Select>
-        </Grid>
-        <Grid item xs={6} style={{ padding: '10px' }}>
-          {/* Add content for the right half of the page */}
-        </Grid>
-      </Grid>
-    </>
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? 'View Your Query' : 'Next'}
+            </Button>
+          </Box>
+        </>
+      )}
+    </Box>
   );
 }
-
-export default DefineMappings;
 
 DefineMappings.propTypes = {
   userID: PropTypes.number.isRequired,
--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -8,7 +8,9 @@ import { MainContext } from '../MainContext';
 import { cleanJWTToken } from '../Utils/LocalStorageAccessor';
 
 function NavBar() {
-  const { hasLogin, setHasLogin, userID } = useContext(MainContext);
+  const {
+    hasLogin, setHasLogin, userID, userName, isUserAdmin,
+  } = useContext(MainContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -19,8 +21,8 @@ function NavBar() {
 
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+      <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant="h6" component="div">
           {hasLogin ? (
             <RouterLink to={`/home/${userID}`} style={{ textDecoration: 'none', color: 'inherit' }}>
               Home
@@ -31,6 +33,22 @@ function NavBar() {
             </RouterLink>
           )}
         </Typography>
+        {hasLogin && (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Typography align="center">
+            Welcome
+            {' '}
+            <span style={{ fontStyle: 'italic' }}>
+              {isUserAdmin ? 'Mapping Administrator' : 'Data Analyst'}
+            </span>
+          </Typography>
+          <Typography align="center" sx={{ fontWeight: 'bold' }}>
+            &nbsp;
+            {userName}
+          </Typography>
+        </div>
+        )}
+
         {hasLogin ? (
           <Button color="inherit" onClick={handleLogout}>
             Logout
