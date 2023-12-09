e23b15e2af6609bff9384f2051de0bdae3b7a072
Mubai Hua <hmbyxfyj@gmail.com>
2023-12-08 22:37:06 -0800

    add backend documentation

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -40,4 +40,5 @@ urlpatterns = [
     path('api/', include('excelinterface.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
